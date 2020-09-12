0e0463a73b31bd0516f2debcc3247896e04d882c
GameRuiner <markgolovko@gmail.com>
2020-09-12 15:15:48 +0300

    Read image size

--- send_img/send_img.js
+++ send_img/send_img.js
@@ -7,6 +7,8 @@ const canv = document.getElementById('test_canvas');
 const ctx = canv.getContext("2d");
 //a timer that will help us measure apps performance
 var timer; var mlsec = 0;
+let imgWidth = 1920; 
+let imgHeight = 1080;
 
 //function that gets triggered once the user decides to upload an image
 function show_img(upload) {
@@ -14,8 +16,17 @@ function show_img(upload) {
   timer = setInterval(()=> mlsec++, 1);
   //getting this img as a file
   let img_file = upload.files[0];
+   var file, img;
+   if ((file = upload.files[0])) {
+        img = new Image();
+        img.onload = function () {
+              imgWidth = this.width;
+              imgHeight = this.height;
+    };
+   }
+  img.src = URL.createObjectURL(file);
+  console.log(imgWidth,imgHeight)
   //converting it to a bytes array with base64 encoding
-  console.log(typeof img_file);
   to_base64(img_file);
 }
 
@@ -54,13 +65,13 @@ function post_img(b64_img){
         console.log(xhr.responseText);
 
         let vehicles = JSON.parse(xhr.responseText).vehicles
-
+        
         //displaying an img that was received and sent back by the server
         //again, not ideal, might not be JPEG
         var image = new Image();
         image.onload = function() {
-          ctx.drawImage(image, 0, 0, canv.width, canv.height);
-
+          console.log(imgWidth,imgHeight)
+          ctx.drawImage(image, 0, 0, imgWidth, imgHeight);
           for (let i = 0 ; i < vehicles.length; i++){
             let vehicle = vehicles[i];
             ctx.beginPath();
@@ -75,7 +86,10 @@ function post_img(b64_img){
       }
   };
   //serializing our img data
-  var data = JSON.stringify({'image': b64_img, 'size': "12,12"});
+  //var data = JSON.stringify({'image': b64_img, 'size': canv.width +','+canv.height});
+  var data = JSON.stringify({'image': b64_img, 'size': canv.width +','+canv.height});
+  console.log(canv.width +','+canv.height);
+
   //sending request
   xhr.send(data);
 }
